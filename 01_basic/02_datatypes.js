f9008f1fa9b5a718eaa06a8965cef5bf5337d176
Prateekk02 <prateek.kumar2002@gmail.com>
2023-12-02 02:15:31 +0530

    Created 02_datatypes.js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version.
+
+// alert(3+3)  we are using node.js not the browser hence this line gives error. 
+// Follow good code writing habbits.
+
+// Javascript's original documentation --> tc39.es  (ECMA script)
+
+
+// null --> standalone value.
+
+//symbol --> used during react (unique)
+
+//object.
+
+console.log(typeof null);   // returns datatype as object !! 
+console.log(typeof undefined);
