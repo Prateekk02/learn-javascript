fc1d5b5217b92ea902bbe1f9e98490bceab0d442
Prateekk02 <prateek.kumar2002@gmail.com>
2023-12-02 02:02:01 +0530

    Created 01_variable.js

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144422
+let accountEmail = "prateek.kumar2002@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+
+// accountId = 2
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail= "hehasd@gamil.com"
+accountPassword = "15444"
+accountCity = "Satna"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+ Using let instead of var is recommended as var pose block scope and functional scope problem.
+*/
