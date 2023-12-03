507dc86faae3b55f5ec4d09faa828ea59b92e13c
Prateekk02 <prateek.kumar2002@gmail.com>
2023-12-03 06:10:05 +0530

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,22 +1,40 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-let numberInValue = Number(score)
+// let numberInValue = Number(score)
 
-console.log(typeof(numberInValue))
+// console.log(typeof(numberInValue))
 
-let newScore = "33abc";
+// let newScore = "33abc";
 
-let newNoValue = Number(newScore)
+// let newNoValue = Number(newScore)
 
-console.log(typeof(newNoValue));
+// console.log(typeof(newNoValue));
 
-//  Prints NaN.
-console.log((newNoValue));
+// //  Prints NaN.
+// console.log((newNoValue));
 
 /*
 "33" -> 33
 "33abc" -> NaN
 true -> 1 ; false -> 0
 */
+
+ /****************************************Operations*********************************************/
+
+console.log(1+2)    // 3
+
+console.log(1+"2")   // 12
+
+console.log("1" + 2 + 2)  // 122
+
+console.log(1 + 2 + "2")  // 32
+
+console.log(true)   // true
+
+console.log(+true)  // 1
+
+console.log(+"")  // 0
+
+// Prefix and postfix  --> study
